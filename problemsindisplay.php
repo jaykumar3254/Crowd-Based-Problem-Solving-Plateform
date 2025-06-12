d2a69c872a4e3bf773cfaae042adf3399ff7f2be
jaykumar3254 <137028327+jaykumar3254@users.noreply.github.com>
2025-06-12 10:18:22 +0530

    .
    .

--- problemsindisplay.php
+++ problemsindisplay.php
@@ -2,7 +2,7 @@
 include 'db.php';
 
 // Join problems with users table to get username
-$sql = "SELECT p.title, p.description, u.user_id
+$sql = "SELECT p.title, p.description, u.Name
         FROM problems p
         JOIN userdb u ON p.user_id = u.user_id
         ORDER BY p.content_id DESC";
